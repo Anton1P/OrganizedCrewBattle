a3cb5f2e128a204e7a77ba4aa89e3665fafd3a37
Antonin zBlackneight <antonin.parede@gmail.com>
2024-10-22 11:21:45 +0200

    rest local

--- bddConnexion/bddConnexion.php
+++ bddConnexion/bddConnexion.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "crewbasantonin.mysql.db"; // ou l'adresse de ton serveur de base de données
-$username = "crewbasantonin"; // ton nom d'utilisateur
-$password = "Organizedcrewbattle76"; // ton mot de passe
-$dbname = "crewbasantonin"; // le nom de ta base de données
+$servername = "localhost"; // ou l'adresse de ton serveur de base de données
+$username = "root"; // ton nom d'utilisateur
+$password = ""; // ton mot de passe
+$dbname = "organizedcrewbattle"; // le nom de ta base de données
 
 // Création de la connexion
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -12,5 +12,6 @@ if ($conn->connect_error) {
     die("Échec de la connexion : " . $conn->connect_error);
 }
 
-?>
 
+
+?>
