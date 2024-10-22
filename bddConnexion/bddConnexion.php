6831e57af88ba49be15ed054159f484c46de0442
Antonin zBlackneight <antonin.parede@gmail.com>
2024-10-22 11:43:27 +0200

    reset

---
+++ bddConnexion/bddConnexion.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost"; // ou l'adresse de ton serveur de base de données
+$username = "root"; // ton nom d'utilisateur
+$password = ""; // ton mot de passe
+$dbname = "organizedcrewbattle"; // le nom de ta base de données
+
+// Création de la connexion
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Vérification de la connexion
+if ($conn->connect_error) {
+    die("Échec de la connexion : " . $conn->connect_error);
+}
+
+?>
+
